706058ae0036ef33a37fb52bb846c76b107f44ac
Louis-Lefebvre <louislefebvre1999@gmail.com>
2022-08-20 19:21:03 -0500

    ctx fan-out test

--- reliable_tcp_data_streams/dialcontext_test.go
+++ reliable_tcp_data_streams/dialcontext_test.go
@@ -3,20 +3,21 @@ package ch03
 import (
 	"context"
 	"net"
+	"sync"
 	"syscall"
 	"testing"
 	"time"
 )
 
 func TestDialContext(t *testing.T) {
-	dl := time.Now().Add(5 * time.Second)
+	dl := time.Now().Add(2 * time.Second)
 	ctx, cancel := context.WithDeadline(context.Background(), dl)
 	defer cancel()
 
 	var d net.Dialer
 	d.Control = func(_, _ string, _ syscall.RawConn) error {
 		// Sleep long enough to reach the context's deadline
-		time.Sleep(5*time.Second + time.Millisecond)
+		time.Sleep(2*time.Second + time.Millisecond)
 		return nil
 	}
 	conn, err := d.DialContext(ctx, "tcp", "10.0.0.0:80")
@@ -65,4 +66,60 @@ func TestDialContextCancel(t *testing.T) {
 	if ctx.Err() != context.Canceled {
 		t.Errorf("expected canceled context; actual: %q", ctx.Err())
 	}
+}
+
+func TestDialContextCancelFanOut(t *testing.T) {
+	ctx, cancel := context.WithDeadline(
+		context.Background(),
+		time.Now().Add(10*time.Second),
+	)
+
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+
+	go func() {
+		// Only accepting a single connection.
+		conn, err := listener.Accept()
+		if err == nil {
+			conn.Close()
+		}
+	}()
+
+	dial := func(ctx context.Context, address string, response chan int, id int, wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		var d net.Dialer
+		c, err := d.DialContext(ctx, "tcp", address)
+		if err != nil {
+			return
+		}
+		c.Close()
+
+		select {
+		case <-ctx.Done():
+		case response <- id:
+		}
+	}
+
+	res := make(chan int)
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go dial(ctx, listener.Addr().String(), res, i+1, &wg)
+	}
+
+	response := <-res
+	cancel()
+	wg.Wait()
+	close(res)
+
+	if ctx.Err() != context.Canceled {
+		t.Errorf("expected canceled context; actual: %s", ctx.Err())
+	}
+
+	t.Logf("dialer %d retrieved the resource", response)
 }
