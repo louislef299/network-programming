4b268e5d3604d4c49bdb1245bbc4466c23f501d9
Louis-Lefebvre <louislefebvre1999@gmail.com>
2022-08-21 16:21:09 -0500

    finish pinger

--- reliable_tcp_data_streams/ping.go
+++ reliable_tcp_data_streams/ping.go
@@ -6,11 +6,45 @@ import (
 	"time"
 )
 
+const defaultPingInterval = 30 * time.Second
+
 func Pinger(ctx context.Context, w io.Writer, reset <-chan time.Duration) {
 	var interval time.Duration
 	select {
 	case <-ctx.Done():
 		return
 	case interval = <-reset: // pulled initial interval off res
+	default:
+	}
+	if interval <= 0 {
+		interval = defaultPingInterval
+	}
+
+	timer := time.NewTimer(interval)
+	defer func() {
+		if !timer.Stop() {
+			<-timer.C
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case newInterval := <-reset:
+			if !timer.Stop() {
+				<-timer.C
+			}
+			if newInterval > 0 {
+				interval = newInterval
+			}
+		case <-timer.C:
+			if _, err := w.Write([]byte("ping")); err != nil {
+				// track and act on consecutive timeouts here
+				return
+			}
+		}
+
+		_ = timer.Reset(interval)
 	}
 }
