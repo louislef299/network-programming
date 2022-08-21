e5071e4e0fcaf39a0b4e6572d4f084ae3c262763
Louis-Lefebvre <louislefebvre1999@gmail.com>
2022-08-20 19:10:07 -0500

    add ctx cancel

--- reliable_tcp_data_streams/dialcontext_test.go
+++ reliable_tcp_data_streams/dialcontext_test.go
@@ -36,3 +36,33 @@ func TestDialContext(t *testing.T) {
 		t.Errorf("expected deadline exceeded; actual: %v", ctx.Err())
 	}
 }
+
+func TestDialContextCancel(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	sync := make(chan struct{})
+
+	go func() {
+		defer func() { sync <- struct{}{} }()
+
+		var d net.Dialer
+		d.Control = func(_, _ string, _ syscall.RawConn) error {
+			time.Sleep(time.Second)
+			return nil
+		}
+		conn, err := d.DialContext(ctx, "tcp", "10.0.0.1:80")
+		if err != nil {
+			t.Log(err)
+			return
+		}
+
+		conn.Close()
+		t.Error("connection did not timeout")
+	}()
+
+	cancel()
+	<-sync
+
+	if ctx.Err() != context.Canceled {
+		t.Errorf("expected canceled context; actual: %q", ctx.Err())
+	}
+}
